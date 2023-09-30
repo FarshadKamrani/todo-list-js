47c020496f083ef0c905ae71bc6aec95365674ad
Farshad Kamrani <140939163+FarshadKamrani@users.noreply.github.com>
2023-09-30 09:17:06 +0100

    just add visual features

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,7 +22,6 @@ if (saveTheme == "dark") {
     themeIcon.classList.add("fa-sun", "text-warning");
 }
 
-
 function addToDo() {
     event.preventDefault();
     const add = document.getElementById("todoInput").value;
@@ -40,23 +39,34 @@ function createTodoElement(add, id = '') {
 
     const parentDiv = document.createElement("div");
     parentDiv.setAttribute('id', id);
+    parentDiv.setAttribute("data-status", "ondoing");
     parentDiv.classList.add("row", "g-3", "my-2");
 
     const divChild1 = document.createElement("div");
-    divChild1.classList.add("col-12", "col-md-10");
+    divChild1.classList.add("col-12", "col-lg-9");
     const todoInfo = document.createElement("div");
     todoInfo.classList.add("alert", "alert-secondary", "mb-0");
     todoInfo.innerHTML = add;
 
     const divChild2 = document.createElement("div");
-    divChild2.classList.add("col-12", "col-md-2");
+    divChild2.classList.add("col-12", "col-lg-3");
     const btnParent = document.createElement("div");
-    btnParent.classList.add("d-flex", "justify-content-center", "align-items-center", "gap-2", "h-100");
+    btnParent.classList.add("d-flex", "justify-content-center", "align-items-center", "gap-1", "h-100");
+
+    const btnDone = document.createElement("button");
+    btnDone.classList.add("btn", "btn-success");
+    btnDone.setAttribute("onclick", "doneTodo(" + id + ",this)");
+    btnDone.innerHTML = "Done";
 
     const btnRemove = document.createElement("button");
     btnRemove.classList.add("btn", "btn-danger");
     btnRemove.setAttribute("onclick", "removeTodo(" + id + ")");
     btnRemove.innerHTML = "Remove";
+
+    const btnEdit = document.createElement("button");
+    btnEdit.classList.add("btn", "btn-info");
+    btnEdit.setAttribute("onclick", "editTodo(" + id + ")");
+    btnEdit.innerHTML = "Edit";
 
     mainSection.appendChild(parentDiv);
     parentDiv.appendChild(divChild1);
@@ -65,12 +75,48 @@ function createTodoElement(add, id = '') {
     divChild1.appendChild(todoInfo);
 
     divChild2.appendChild(btnParent);
+    btnParent.appendChild(btnDone);
     btnParent.appendChild(btnRemove);
+    btnParent.appendChild(btnEdit);
 }
 
 function removeTodo(id) {
     document.getElementById(id).remove();
     popFromLocalStorage('todos', id);
+}
+
+function doneTodo(id, btnElement) {
+    const parentElement = document.getElementById(id);
+    const doneTask = parentElement.children[0].children[0];
+    doneTask.classList.remove("alert-secondary");
+    doneTask.classList.add("alert-success", "text-decoration-line-through");
+    parentElement.setAttribute("data-status", "done");
+    btnElement.remove();
+}
+
+function editTodo(id) {
+    const parentElement = document.getElementById(id);
+    const editTask = parentElement.children[0].children[0];
+    const inputCreate = document.createElement("input");
+    inputCreate.classList.add("form-control", "my-2");
+    inputCreate.value = editTask.innerHTML;
+    editTask.replaceWith(inputCreate);
+}
+
+function filterTodo(requested) { //done || on_doing || all
+    const finishedTasks = document.getElementById("mainSection").children;
+    for (let i = 0; finishedTasks.length > i; i++) {
+        if (requested == "all") {
+            finishedTasks[i].classList.remove("d-none");
+        } else {
+            if (finishedTasks[i].getAttribute("data-status") == requested) {
+                finishedTasks[i].classList.remove("d-none");
+            } else {
+                finishedTasks[i].classList.add("d-none");
+            }
+        }
+    }
+
 }
 
 function appendToLocalStorage(itemName, newKey, newValue) {
